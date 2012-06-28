16d683e582ec6794ef507128735b3561aeac27ff
junikorn <blazejwolanczyk@gmail.com>
2012-06-28 12:52:02 +0200

    plum

--- controler.js
+++ controler.js
@@ -555,7 +555,9 @@ function Controler(url, gui){
 				success: fun_success || function(res, status, jqXHR){
 					console.group("AJAX QUERY RESULTS:");
 					console.info("Loaded data from "+sUrl+" with status: "+status);
+					console.group("Response:");
 					console.log(res);
+					console.groupEnd();
 					console.groupEnd();
 				},
 				error: fun_error || function(jqXHR, status, e){
@@ -577,7 +579,9 @@ function Controler(url, gui){
 				success: fun_success || function(res, status, jqXHR){
 					console.group("AJAX QUERY RESULTS:");
 					console.info("Send data to "+sUrl+" with status: "+status);
+					console.group("Response:");
 					console.log(res);
+					console.groupEnd();
 					console.groupEnd();
 				},
 				error: fun_error || function(jqXHR, status, e){
