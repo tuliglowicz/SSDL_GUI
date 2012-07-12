d5a8f0d042e31950cf11151e114fab11af6286d4
junikorn <blazejwolanczyk@gmail.com>
2012-07-12 11:35:12 +0200

    logger private vars

--- controler.js
+++ controler.js
@@ -83,7 +83,6 @@ function Controler(url, gui){
 		*/
 		var h = paper.height,
 			dId = "#console_" + pf,
-			button = paper.set(),
 			bPath = 'M'+ (paper.width - 170) + ' 0 Q' + (paper.width - 170) + ' 25 '
 			 + (paper.width - 145) +' 25 L' + (paper.width - 45) + ' 25 Q' 
 			 + (paper.width - 20) + ' 25 ' + (paper.width - 20) + ' 0 Z',
@@ -92,91 +91,77 @@ function Controler(url, gui){
 				fill : "#222",
 				"fill-opacity": .75
 			});
-		button.push(buttonBG);
 		//images
 		var iImg = paper.image('images/info.png', paper.width - 152, 4, 15, 15),
 			wImg = paper.image('images/warning.png', paper.width - 112, 4, 15, 15),
 			eImg = paper.image('images/error.png', paper.width - 72, 4, 15, 15);
-		button.push(iImg);
-		button.push(wImg);
-		button.push(eImg);
 		//counters
 		var iCounter = paper.text(paper.width - 125, 11, "0").attr({fill: "white"}),
 			wCounter = paper.text(paper.width - 85, 11, "0").attr({fill: "yellow"}),
 			eCounter = paper.text(paper.width - 45, 11, "0").attr({fill: "orange"});
-		button.push(iCounter);
-		button.push(wCounter);
-		button.push(eCounter);
 		var buttonMask = paper.path(bPath).attr({
 			fill : "#222",
 			"fill-opacity": 0.0
 		});
+		//private variables
+		var lId = dId,
+			counter = [0, 0, 0],
+			mask = buttonMask,
+			bImgs = [iImg, wImg, eImg],
+			bCount = [iCounter, wCounter, eCounter],
+			buttonBG = buttonBG,
+			bGlow = null,
+			animation = null,
+			curElCount = 0,
+			actionTaken = false;
 		//console object
 		var obj = {
-			lId : dId,
-			counter : [0, 0, 0],
-			mask : buttonMask,
-			button : button,
-			bImgs : [iImg, wImg, eImg],
-			bCount : [iCounter, wCounter, eCounter],
-			buttonBG : buttonBG,
-			bGlow : null,
-			animation : null,
-			curElCount : 0,
-			actionTaken : false,
 			info : function(text, title){
 				text = text || "(an empty string)";
 				if(title) text = "<b>"+title+"</b><br/>" + text;
-				this.counter[0]++;
-				this.bCount[0].remove();
-				this.bCount[0] = paper.text(paper.width - 125, 11, this.counter[0]).attr({fill: "white"});
-				this.button.push(this.bCount[0]);
-				this.mask.toFront();
+				counter[0]++;
+				bCount[0].remove();
+				bCount[0] = paper.text(paper.width - 125, 11, counter[0]).attr({fill: "white"});
+				mask.toFront();
 				//here adding to div and dTabs
 				this.addMessage(text, 0);
 			},
 			warning : function(text, title){
 				text = text || "(an empty string)";
 				if(title) text = "<b>"+title+"</b><br/>" + text;
-				this.counter[1]++;
-				this.bCount[1].remove();
-				this.bCount[1] = paper.text(paper.width - 85, 11, this.counter[1]).attr({fill: "yellow"});
-				this.button.push(this.bCount[1]);
-				if(this.bGlow) this.bGlow.remove();
-				this.bGlow = this.buttonBG.glow().attr({fill : "#FFFF00"});
-				this.mask.toFront();
+				counter[1]++;
+				bCount[1].remove();
+				bCount[1] = paper.text(paper.width - 85, 11, counter[1]).attr({fill: "yellow"});
+				if(bGlow) bGlow.remove();
+				bGlow = buttonBG.glow().attr({fill : "#FFFF00"});
+				mask.toFront();
 				//here adding to div and dTabs
 				this.addMessage(text, 1);
 			},
 			error : function(text, title){
 				text = text || "(an empty string)";
 				if(title) text = "<b>"+title+"</b><br/>" + text;
-				this.counter[2]++;
-				this.bCount[2].remove();
-				this.bCount[2] = paper.text(paper.width - 45, 11, this.counter[2]).attr({fill: "orange"});
-				this.button.push(this.bCount[2]);
-				if(this.bGlow) this.bGlow.remove();
-				this.bGlow = this.buttonBG.glow(10, true).attr({fill : "#FFFF00"});
-				this.mask.toFront();
+				counter[2]++;
+				bCount[2].remove();
+				bCount[2] = paper.text(paper.width - 45, 11, counter[2]).attr({fill: "orange"});
+				if(bGlow) bGlow.remove();
+				bGlow = buttonBG.glow(10, true).attr({fill : "#FFFF00"});
+				mask.toFront();
 				//here adding to div and dTabs
 				this.addMessage(text, 2);
 				//pulse
-				var fade = function(o){
-					if(o.buttonBG.attr("fill-opacity")==1){
-						o.buttonBG.animate({"fill-opacity": 0.4}, 700);
+				var fade = function(){
+					if(buttonBG.attr("fill-opacity")==1){
+						buttonBG.animate({"fill-opacity": 0.4}, 700);
 					}else{
-						o.buttonBG.animate({"fill-opacity": 1}, 700);
+						buttonBG.animate({"fill-opacity": 1}, 700);
 					}
 				};
-				if(this.animation) clearInterval(this.animation);
-				this.animation = setInterval((function(that){
-					return function(){
-						fade(that);
-					}
-				})(this),750);
+				if(animation) clearInterval(animation);
+				animation = setInterval(fade, 750);
 			},
 			addMessage : function(message, priority){
-				this.curElCount++;
+				curElCount++;
 				var divId = "console_row_" + this.curElCount;
 				var colors = ['#FAFAFF','#FFFFE0','#FFFAFA'];
 				var txtColors = ['white','yellow','orange'];
@@ -194,50 +179,48 @@ function Controler(url, gui){
 				divString.push("<td valign='top' style='float: left;'>");
 				divString.push(message);
 				divString.push("</td><td valign='top' style='width: 20px;'><div id='cCancel_");
-				divString.push(this.curElCount);
+				divString.push(curElCount);
 				divString.push("'><img src='images/cancel.png' style='padding-left: 2px; padding-top: 3px;'/></div></td></tr></table>");
 				divString = divString.join("");
-				$(divString).prependTo($(this.lId));
+				$(divString).prependTo($(lId));
 				var that = this;
-				var nr = this.curElCount;
+				var nr = curElCount;
 				var delId = "#cCancel_"+this.curElCount;
-				$(delId).click((function(that){
-					return function(){
-						that.actionTaken = true;
-						that.counter[priority]--;
-						that.bCount[priority].remove();
-						that.bCount[priority] = paper.text(paper.width - (125 - (priority * 40)), 11, that.counter[priority]).attr({fill: txtColors[priority]});
-						that.button.push(that.bCount[priority]);
-						that.mask.toFront();
-						delId = "#console_row_" + nr;
-						$(delId).remove();
-					}
-				})(this));
+				$(delId).click(function(){
+					actionTaken = true;
+					counter[priority]--;
+					bCount[priority].remove();
+					bCount[priority] = paper.text(paper.width - (125 - (priority * 40)), 11, counter[priority]).attr({fill: txtColors[priority]});
+					button.push(bCount[priority]);
+					mask.toFront();
+					delId = "#console_row_" + nr;
+					$(delId).remove();
+				});
 			}
 		};
 		//event handling
 		$(dId).click(function(){
-			if(obj.actionTaken){
-				obj.actionTaken = false;
+			if(actionTaken){
+				actionTaken = false;
 			}else{
 				$(dId).animate({
 					height: 0
 				});
 			}
 		});
-		obj.mask.click(function(){
+		mask.click(function(){
 			$(dId).animate({
 				height: h
 			});
 		});
-		obj.mask.mouseover(function(){
-			if(obj.bGlow) obj.bGlow.remove();
-			if(obj.animation) clearInterval(obj.animation);
-			obj.buttonBG.animate({"fill-opacity": 0.75}, 500);
-			obj.bGlow = obj.buttonBG.glow();
+		mask.mouseover(function(){
+			if(bGlow) bGlow.remove();
+			if(animation) clearInterval(animation);
+			buttonBG.animate({"fill-opacity": 0.75}, 500);
+			bGlow = buttonBG.glow();
 		});
-		obj.mask.mouseout(function(){
-			obj.bGlow.remove();
+		mask.mouseout(function(){
+			bGlow.remove();
 		});
 		return obj;
 	}
