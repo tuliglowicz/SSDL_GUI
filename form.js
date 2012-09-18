fdf5473e8a257496b900c0a5382f15b5dbbcbc72
wtuliglowicz <w.tuliglowicz@gmail.com>
2012-09-18 12:10:25 +0200

    poprawa w funkcji removeUndefinedElements()

--- form.js
+++ form.js
@@ -710,18 +710,39 @@
 			},
 		//usuwanie undefinedów z tablic i/o/nfp
 			removeUndefinedElements: function removeUndefinedElements(){
-				for(var i in this.funcDescJSON.inputs)
-					if(!this.funcDescJSON.inputs[i])
-						this.funcDescJSON.inputs.splice(i, 1);
-				for(var i in this.funcDescJSON.outputs)
-					if(!this.funcDescJSON.outputs[i])
-						this.funcDescJSON.outputs.splice(i, 1);
-				for(var i in this.resultJSON.nonFunctionalDescription)
-					if(!this.resultJSON.nonFunctionalDescription[i]) 
-						this.resultJSON.nonFunctionalDescription.splice(i, 1);
-				for(var i in this.funcDescJSON.serviceClasses)
-					if(!this.funcDescJSON.serviceClasses[i]) 
-						this.funcDescJSON.serviceClasses.splice(i, 1);
+				var inputs = this.funcDescJSON.inputs,
+					outputs = this.funcDescJSON.outputs,
+					nonFunctionalDescription = this.resultJSON.nonFunctionalDescription,
+					serviceClasses = this.funcDescJSON.serviceClasses,
+					tmp
+				;
+				tmp = [];
+				for(var i in inputs){
+					if( inputs[ i ] )
+						tmp.push(inputs[ i ]);
+				}
+				this.funcDescJSON.inputs = tmp;
+
+				tmp = [];
+				for(var i in outputs){
+					if( outputs[ i ] )
+						tmp.push(outputs[ i ]);
+				}
+				this.funcDescJSON.outputs = tmp;
+
+				tmp = [];
+				for(var i in nonFunctionalDescription){
+					if( nonFunctionalDescription[ i ] ) 
+						tmp.push( nonFunctionalDescription[ i ] );
+				}
+				this.resultJSON.nonFunctionalDescription = tmp;
+
+				tmp = [];
+				for(var i in serviceClasses){
+					if( serviceClasses[ i ] ) 
+						tmp.push( serviceClasses[ i ] );
+				}
+				this.funcDescJSON.serviceClasses = tmp;
 			},
 		//obsługa zmiennych określających zaznaczone elementy w tabelach
 			getSelectedInputIndex: function getSelectedInputIndex(){
